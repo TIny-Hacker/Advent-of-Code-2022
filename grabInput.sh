458e67b2bd4bdde0bac4fb02da015bf09e15465e
TIny_Hacker <thetiniesthacker@gmail.com>
2022-11-30 09:09:28 -0500

    Add some error checking to the input script

--- grabInput.sh
+++ grabInput.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ ! -f "cookies.txt" ]] # Ensure the user has a cookies.txt file
+then
+    echo No cookies.txt file. Please generate a cookies.txt file and put it in the same directory as this script.
+    exit 1
+fi
+
 echo Prepare directories
 
 if [[ ! -f "Day$2" ]] # Check if the directory has already been created, if not, create it
@@ -22,3 +28,6 @@ rm input
 echo Convert to 8xv
 
 convbin -j bin -k 8xv -i input.txt -o Input.8xv -n Input    # Convert to appvar
+
+echo Success!
+exit 0
