7a694ac0132557788e2215338764c3ab5bb46cbe
TIny_Hacker <thetiniesthacker@gmail.com>
2022-11-30 11:57:42 -0500

    Better error checking
    Yes this is my first time ever doing bash

--- grabInput.sh
+++ grabInput.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-if [[ ! -f "cookies.txt" ]] # Ensure the user has a cookies.txt file
+YEAR=$1
+DAY=$2
+
+if [[ $# -ne 2 ]]
 then
-    echo No cookies.txt file. Please generate a cookies.txt file and put it in the same directory as this script.
+    echo "Incorrect number of arguments supplied. Proper syntax is ./grabInput.sh <year> <day>"
     exit 1
 fi
 
-echo Prepare directories
-
-if [[ ! -f "Day$2" ]] # Check if the directory has already been created, if not, create it
+if [[ ! -f "cookies.txt" ]] # Ensure the user has a cookies.txt file
 then
-    mkdir Day$2
+    echo "No cookies.txt file. Please generate a cookies.txt file and put it in the same directory as this script."
+    exit 1
+fi
+
+echo "Preparing directories..."
+
+if [[ ! -d "Day$DAY" ]] # Check if the directory has already been created, if not, create it
+then
+    mkdir Day$DAY
 fi
 
 cd Day$2
 
-echo Download input
+echo "Downloading input..."
 
-wget --load-cookies=../cookies.txt "https://adventofcode.com/$1/day/$2/input"   # Download latest input
+wget --load-cookies=../cookies.txt "https://adventofcode.com/$YEAR/day/$DAY/input"   # Download latest input
 
-echo Convert newlines
+echo "Converting newlines to null..."
 
 tr '\n' '\0' < input > input.txt   # Zero terminate newlines
 
 rm input
 
-echo Convert to 8xv
+echo "Converting output to 8xv..."
 
 convbin -j bin -k 8xv -i input.txt -o Input.8xv -n Input    # Convert to appvar
 
